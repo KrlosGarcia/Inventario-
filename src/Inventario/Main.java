7eb904e2d95c1729bffd8c67e4913bc56862f053
KrlosGarcia <ccgarciadavid@hotmail.com>
2020-08-10 21:28:41 -0500

    Tarea del Inventario

---
+++ src/Inventario/Main.java
@@ -0,0 +1,22 @@
+package Inventario;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Profe no sé como hacer este");
+		System.out.println("Le he fallado :c");
+		System.out.println("Lo siento ...");
+		
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		
+		
+		System.out.println("SYNTAX ERROR #$%&");
+
+	}
+
+}
